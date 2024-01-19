1fbc83ac6ecb3da7440984e80a8fd782d9749744
varadaraj772 <varadaraj277@gmail.com>
2024-01-19 13:30:21 +0530

    change in config

---
+++ config.mjs
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXl1m5z8BgrSKPAu1QQ8g7xPAVmIxcYPo",
+  authDomain: "pass-manager-65b35.firebaseapp.com",
+  databaseURL: "https://pass-manager-65b35-default-rtdb.firebaseio.com",
+  projectId: "pass-manager-65b35",
+  storageBucket: "pass-manager-65b35.appspot.com",
+  messagingSenderId: "247300599673",
+  appId: "1:247300599673:web:b25a9d97b69d2932bac9a8",
+  measurementId: "G-5JGCECHEYF",
+};
